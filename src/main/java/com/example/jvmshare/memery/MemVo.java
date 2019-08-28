4a1cbc6e19265302ec5deb4ef6c3afca4339fd80
r2dxxc <weiwei.zhuang@siemens.com>
2019-08-28 13:46:11 +0800

    jdk

--- src/main/java/com/example/jvmshare/memery/MemVo.java
+++ src/main/java/com/example/jvmshare/memery/MemVo.java
@@ -1,9 +1,25 @@
 package com.example.jvmshare.memery;
 
-import lombok.Data;
 
-@Data
+
+
 public class MemVo {
     String name;
     Byte[] bytes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(Byte[] bytes) {
+        this.bytes = bytes;
+    }
 }
